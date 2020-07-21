9f34f757d6f5d305f26476ebc15274aaa2b7a20d
JinWoo Kim <znu4847@gmail.com>
2020-07-22 00:25:33 +0900

    init

---
+++ src/main/java/znu/practice/ie/with/webpack/demo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package znu.practice.ie.with.webpack.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+  @GetMapping("/index")
+  public String index() {
+    return "index";
+  }
+}
---
+++ src/test/java/znu/practice/ie/with/webpack/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package znu.practice.ie.with.webpack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
